764b55f18ddc38c896c7fdc6e47ccbe38da2204f
mouryame <mourya.chiranjeevi@gmail.com>
2024-01-17 20:24:32 +0530

    initial commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button/Button'
+import React from 'react'
+
+function Test() {
+  return (
+    <div style={{width:'100vw', height:'100vh'}}>
+        <Button />
+    </div>
+  )
+}
+
+export default Test
